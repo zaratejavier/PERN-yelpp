cb7e8673a7075d60ba23e091ed9dcc9ca437444f
Javier Zarate <zarate.javier.jz@gmail.com>
2020-10-19 21:00:10 -0600

    update and delete call are working with postman

--- server/server.js
+++ server/server.js
@@ -19,13 +19,44 @@ app.get("/api/v1/restaurants", (req, res) => {
 //Get one restaurant
 app.get("/api/v1/restaurants/:id", (req, res) => {
   console.log(req.params)
+  res.status(200).json({
+    state: "sucess",
+    data: {
+      restaurant: "mcdonalds"
+    }
+  })
 })
 
 //create restaurant
 app.post("/api/v1/restaurants", (req, res) => {
   console.log(req.body)
+  res.status(201).json({
+    state: "sucess",
+    data: {
+      restaurant: "mcdonalds"
+    }
+  })
 })
 
+//update restaurant
+app.put("/api/v1/restaurants/:id", (req, res) => {
+  console.log(req.params.id)
+  console.log(req.body)
+  res.status(200).json({
+    state: "sucess",
+    data: {
+      restaurant: "mcdonalds"
+    }
+  })
+})
+
+//delete restaurant
+app.delete("/api/v1/restaurants/:id", (req, res) => {
+  res.status(204).json({
+    status: "sucess"
+  })
+
+})
 
 const port = process.env.PORT || 3003;
 app.listen(port, () => {
