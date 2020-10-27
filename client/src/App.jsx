bfaa8d4c3d2e53914952207dc0aaa63b31383f93
Javier Zarate <zarate.javier.jz@gmail.com>
2020-10-26 21:12:54 -0600

    added routes to each page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Home from './routes/Home';
+import RestaurantdetailPage from './routes/RestaurantdetailPage';
+import UpdatePage from './routes/UpdatePage';
 
 const App = () => {
   return (
     <div>
-      App
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
+          <Route exact path="/restaurants/:id" component={RestaurantdetailPage}/>
+        </Switch>
+      </Router>
     </div>
   )
 }
