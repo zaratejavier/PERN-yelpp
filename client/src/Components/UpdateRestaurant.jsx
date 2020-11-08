5ad50070cdb3734abf65b719f84b8bcd32351d78
Javier Zarate <zarate.javier.jz@gmail.com>
2020-11-07 19:23:29 -0700

    fixed state bug

--- client/src/Components/UpdateRestaurant.jsx
+++ client/src/Components/UpdateRestaurant.jsx
@@ -1,45 +1,45 @@
-import react, { useState } from "react"
-import { useParams } from "react-router-dom"
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const UpdateRestaurant = () => {
   const { id } = useParams();
-  const { name, setName } = useState("")
-  const { location, setLocation } = useState("")
-  const {price_range, setPrice_range} = useState("")
+  const [ name, setName ] = useState("");
+  const [ location, setLocation ] = useState("");
+  const [ price_range, setPrice_range ] = useState("");
 
   return (
     <div>
       <form action="">
         <div className="form-group">
           <label htmlFor="name">Name</label>
-          <input
+           <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            id="name"
             className="form-control"
             type="text"
-            id="name"
-            value={name}
-            onChange={e => setName(e.target.value)}
           />
         </div>
 
         <div className="form-group">
           <label htmlFor="location">Location</label>
           <input
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
             className="form-control"
             type="text"
             id="location"
-            value={location}
-            onChange={e => setLocation(e.target.value)}
           />
         </div>
 
         <div className="form-group">
           <label htmlFor="price_range">Price Range</label>
           <input
+            value={price_range}
+            onChange={(e) => setPrice_range(e.target.value)}
             className="form-control"
             type="number"
             id="price_range"
-            value={price_range}
-            onChange={e => setPrice_range(e.target.value)}
           />
         </div>
         <button className="btn btn-primary">Submit</button>
