def7616e527f49d14b62efb4cda1249e956f3448
Javier Zarate <zarate.javier.jz@gmail.com>
2020-11-09 21:20:31 -0700

    restaurant view is working

--- client/src/Context/RestaurantsContext.js
+++ client/src/Context/RestaurantsContext.js
@@ -4,13 +4,20 @@ export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = (props) => {
   const [restaurants, setRestaurants] = useState([])
+  const [selectedRestaurant, setSelectRestaurant] = useState(null)
 
   const addRestaurants = (restaurant) => {
     setRestaurants([...restaurants, restaurant])
   }
 
   return (
-    <RestaurantsContext.Provider value={{restaurants, setRestaurants, addRestaurants}}>
+    <RestaurantsContext.Provider value={{
+      restaurants,
+      setRestaurants,
+      addRestaurants,
+      selectedRestaurant,
+      setSelectRestaurant,
+    }}>
       {props.children}
     </RestaurantsContext.Provider>
   )
--- client/src/routes/RestaurantdetailPage.jsx
+++ client/src/routes/RestaurantdetailPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import RestaurantFinder from '../apis/RestaurantFinder'
+import { RestaurantsContext } from '../Context/RestaurantsContext'
 
 const RestaurantdetailPage = () => {
+  const { id } = useParams();
+  const { selectedRestaurant, setSelectRestaurant } = useContext(RestaurantsContext);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get(`/${id}`);
+        setSelectRestaurant(response.data.data.restaurant);
+      } catch(err) {
+        console.log(err)
+      }
+    }
+    fetchData()
+  },[])
+
   return (
     <div>
-      Restaurant Detail Page
+    {selectedRestaurant && selectedRestaurant.name}
     </div>
   )
 }
